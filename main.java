a4d5af324cddf3dd434a419c9f56fc3a4f0b315a
erickdrummer182 <107373968+erickdrummer182@users.noreply.github.com>
2025-02-24 19:59:16 -0300

    Versão final
    Versão final do codigo

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Versão 2: Melhorando o código");
+        System.out.println("Versão 3: Função adicionada");
+        saudacao();
+    }
+
+    public static void saudacao() {
+        System.out.println("Olá, usuário!");
     }
 }
